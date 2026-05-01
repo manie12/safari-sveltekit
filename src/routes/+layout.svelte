42c3595919675f382f6e1a745ffe1212b2b32836
kipkurui <jacobkip254@gmail.com>
2026-05-01 09:32:52 +0300

    added versel visibilty

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-  import Header from '$lib/components/Header.svelte';
-  import Footer from '$lib/components/Footer.svelte';
-  import '../app.css';
+  import Header from "$lib/components/Header.svelte";
+  import Footer from "$lib/components/Footer.svelte";
+  import "../app.css";
+  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+
+  injectSpeedInsights();
 </script>
 
 <svelte:head>
